f3a5cfbd8ab86c68e531870b90d62942e4cb902a
Joe Prathap P J <joeprathappj@gmail.com>
2022-12-17 16:50:02 +0530

    creator check

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -51,7 +51,36 @@ export default function Profile() {
                      ):<h1>Loading...</h1>}
                 
                  <div className='flex text-[0.6rem] md:text-[1rem] flex-col gap-2'>
-                 <h1>Name: {studentDetails ? studentDetails.name : <span className='animate-pulse'>loading..</span>}</h1>
+                 
+                <h1>{studentDetails ? (
+                  <span className='flex  items-center gap-1'>
+                     <h1>
+
+                     Name: 
+                     </h1>
+                    {studentDetails.name}
+                    {studentDetails.creator ? (
+                                   <svg
+                                   viewBox="0 0 30 30"
+                                   aria-label="Verified account"
+                                   role="img"
+                                   className="r-1cvl2hr r-4qtqp9 r-yyyyoo r-1xvli5t r-f9ja8p r-og9te1 r-bnwqim r-1plcrui r-lrvibr"
+                                   data-testid="icon-verified"
+                                   width={16}
+                                   height={16}
+                                 
+                                 >
+                                   <g>
+                                     <path
+                                       fill="#FFAF40"
+                                       d="M27.814 15c0 -1.788 -1.101 -3.339 -2.736 -4.176 0.576 -1.736 0.249 -3.624 -1.014 -4.886s-3.15 -1.585 -4.886 -1.014c-0.825 -1.639 -2.389 -2.736 -4.176 -2.736s-3.339 1.101 -4.164 2.736c-1.749 -0.576 -3.639 -0.249 -4.899 1.014s-1.575 3.15 -0.999 4.886c-1.639 0.836 -2.751 2.389 -2.751 4.176s1.113 3.339 2.751 4.176c-0.576 1.736 -0.261 3.624 0.999 4.886s3.15 1.575 4.886 1.014c0.836 1.639 2.389 2.736 4.176 2.736s3.351 -1.101 4.176 -2.736c1.736 0.564 3.624 0.249 4.886 -1.014s1.585 -3.15 1.014 -4.886c1.639 -0.836 2.736 -2.389 2.736 -4.176zm-14.637 5.25L8.499 15.576l1.764 -1.776 2.826 2.826 6 -6.536 1.839 1.701 -7.749 8.464z"
+                                     />
+                                   </g>
+                                 </svg>
+                    ):''}
+                  </span>
+                  ) : <span className='animate-pulse'>loading..</span>}</h1>
+                
                  <h1>Dept & Year : {studentDetails ? studentDetails.department+" - " + studentDetails.year+"nd Year" : <span className='animate-pulse'>loading..</span>} </h1>
                  <h1>Roll No: {studentDetails? studentDetails.rollNo :<span className='animate-pulse'>loading..</span>}</h1>
                  <h1>Section: {userDetails ? userDetails.class : <span className='animate-pulse'>loading..</span>}</h1>
